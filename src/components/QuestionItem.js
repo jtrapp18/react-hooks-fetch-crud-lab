346df5aed0cb9a91d92f7a11173c917b43525068
+ <jacqueline.trapp18@gmail.com>
2024-11-21 17:58:40 -0500

    lab complete

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, updateQuestion, removeQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,41 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleUpdate(event) {
+
+    const updatedObj = {
+      ...question,
+      correctIndex: event.target.value
+    }
+
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify(updatedObj)
+    })
+    .then(res=>res.json())
+    .then(updateQuestion(question));
+  }
+
+  function handleDelete(event) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(res => res.json())
+      .then(() => removeQuestion(question));
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleUpdate}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,60 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+  console.log("list", questions)
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+
+    fetch("http://localhost:4000/questions")
+    .then(res=>res.json())
+    .then(questions=>{
+      console.log(questions)
+      setQuestions(questions);
+      setIsLoaded(true);
+    })
+
+  }, [])
+
+  function updateQuestion(question) {
+
+    setQuestions(prevQuestions=> 
+      prevQuestions.map(prevQuestion=> {
+        if (prevQuestion.id===question.id) {
+          return question
+        }
+        else {
+          return prevQuestion
+        }
+      })
+    )
+    console.log("updated", question);
+  }
+
+  function removeQuestion(question) {
+    
+    setQuestions(prevQuestions=> 
+      prevQuestions.filter(q=> q.id!==question.id)
+    )
+
+    console.log("deleted", question);
+  }
+  if (!isLoaded) return <p>Loading...</p>;
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map(question=> 
+          <QuestionItem
+            key={question.id}
+            question={question}
+            updateQuestion={updateQuestion}
+            removeQuestion={removeQuestion}
+          />
+        )}
+      </ul>
     </section>
   );
 }
